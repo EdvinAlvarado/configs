6509724df79915c5976fac4a4065a04e1180f75e
EdvinAlvarado <ealvarado301@gmail.com>
2021-10-10 11:38:31 -0500

    only copy if updated

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
-cp ~/.zshrc .zshrc
-cp ~/.vimrc .vimrc
-cp ~/.config/nvim/init.vim .config/nvim/init.vim
+cp -u ~/.zshrc .zshrc
+cp -u ~/.vimrc .vimrc
+cp -rfu ~/.config/nvim/init.vim .config/nvim/init.vim
 cp -rfu ~/.config/ranger/rifle.conf .config/ranger/rifle.conf
