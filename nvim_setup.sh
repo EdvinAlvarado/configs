f11d1d491df9f13721e91f13489fd18b4a7c1b02
EdvinAlvarado <ealvarado301@gmail.com>
2021-10-24 16:41:50 -0500

    fixing typo

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -3,5 +3,5 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/.vimrc
-cp -p ./.config/nvim/init.vim ~/.config/nvim/init.vim
+cp -p .config/nvim/init.vim ~/.config/nvim/init.vim
 nvim -c ':PlugInstall'
