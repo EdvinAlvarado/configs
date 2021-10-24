b456ecc089b1d797e6c15ae5e143afe68fa74ed0
EdvinAlvarado <ealvarado301@gmail.com>
2021-10-24 16:33:02 -0500

    updated backup and made a recover

--- backup.sh
+++ backup.sh
@@ -1,4 +1,8 @@
+# Home
 cp -u ~/.zshrc .zshrc
 cp -u ~/.vimrc .vimrc
 cp -rfu ~/.config/nvim/init.vim .config/nvim/init.vim
 cp -rfu ~/.config/ranger/rifle.conf .config/ranger/rifle.conf
+
+# Root
+cp -rfu /etc/doas.conf etc/doas.conf
---
+++ recover.sh
@@ -0,0 +1,8 @@
+# Home
+cp -u .zshrc ~/.zshrc
+cp -u .vimrc ~/.vimrc
+cp -rfu .config/nvim/init.vim ~/.config/nvim/init.vim
+cp -rfu .config/ranger/rifle.conf ~/.config/ranger/rifle.conf
+
+# Root
+# doas cp -rfu etc/doas.conf /etc/doas.conf
