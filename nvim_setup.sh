34c6399f2b7fc9ca102e43df5e46d82e5cc7d70a
EdvinAlvarado <ealvarado301@gmail.com>
2021-10-24 17:03:58 -0500

    fixed nvim setup

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/.vimrc
 cp -p .config/nvim/init.vim ~/.config/nvim/init.vim
 nvim -c ':PlugInstall'
