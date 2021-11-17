b14febf380460b5bca6c30c05e90f0eec532b19a
EdvinAlvarado <ealvarado301@gmail.com>
2021-11-16 21:17:38 -0600

    generalized for different outpoints

--- Arch/btrfs/luks_btrfs_partioner.sh
+++ Arch/btrfs/luks_btrfs_partioner.sh
@@ -1,47 +1,47 @@
-# $1 == device
-# $2 == distro
+# $1 == mount
+# $2 == device
+# $3 == distro
 
 # create
-cryptsetup --type luks1 --label="CRYPTROOT" luksFormat $1
-cryptsetup open $1 cryptroot
+cryptsetup --type luks1 --label="CRYPTROOT" luksFormat $2
+cryptsetup open $2 cryptroot
 mkfs.btrfs -L ROOT /dev/mapper/cryptroot
 
-mount /dev/mapper/cryptroot /mnt
-btrfs subvolume create /mnt/@
-btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@snapshots
-btrfs subvolume create /mnt/@swap
+mount /dev/mapper/cryptroot $1
+btrfs subvolume create $1/@
+btrfs subvolume create $1/@home
+btrfs subvolume create $1/@snapshots
+btrfs subvolume create $1/@swap
 
-if [ $2 == "arch" ]
+if [ $3 == "arch" ]
 then
-	btrfs subvolume create /mnt/@var_log
-	mkdir -p /mnt/@/var/cache/pacman
-	btrfs subvolume create /mnt/@/var/cache/pacman/pkg
+	btrfs subvolume create $1/@var_log
+	mkdir -p $1/@/var/cache/pacman
+	btrfs subvolume create $1/@/var/cache/pacman/pkg
 fi
 
 # mount
 umount /mnt
-mount -o compress=zstd,subvol=@ /dev/mapper/cryptroot /mnt
+mount -o compress=zstd,subvol=@ /dev/mapper/cryptroot $1
 mkdir /mnt/{home,.snapshots,swap}
-mount -o compress=zstd,subvol=@home /dev/mapper/cryptroot /mnt/home
-mount -o compress=zstd,subvol=@snapshots /dev/mapper/cryptroot /mnt/.snapshots
-mount -o subvol=@swap /dev/mapper/cryptroot /mnt/swap
+mount -o compress=zstd,subvol=@home /dev/mapper/cryptroot $1/home
+mount -o compress=zstd,subvol=@snapshots /dev/mapper/cryptroot $1/.snapshots
+mount -o subvol=@swap /dev/mapper/cryptroot $1/swap
 
-if [ $2 == "arch" ]
+if [ $3 == "arch" ]
 then
-	mkdir -p /mnt/var/log
-	mount -o compress=zstd,subvol=@var_log /dev/mapper/cryptroot /mnt/var/log
+	mkdir -p $1/var/log
+	mount -o compress=zstd,subvol=@var_log /dev/mapper/cryptroot $1/var/log
 fi
 
 
 # swap
-mkdir /mnt/{efi,recovery}
-mount /dev/sda1 /mnt/efi
-mount /dev/sda2 /mnt/recovery
-cryptsetup luksHeaderBackup /dev/sda3 --header-backup-file /mnt/recovery/LUKS_header_backup.img
+mkdir $1/{efi,recovery}
+mount /dev/sda1 $1/efi
+mount /dev/sda2 $1/recovery
+cryptsetup luksHeaderBackup $2 --header-backup-file $1/recovery/LUKS_header_backup.img
 
 # crypto keyfile
 dd bs=512 count=4 if=/dev/random of=/crypto_keyfile.bin iflag=fullblock
 chmod 600 /crypto_keyfile.bin
-chmod 600 /boot/initramfs-linux*
-cryptsetup luksAddKey /dev/sda3 /crypto_keyfile.bin
+cryptsetup luksAddKey $2 /crypto_keyfile.bin
