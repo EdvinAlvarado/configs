06f4d4f68ab2e3c945882d90a612da580e462feb
EdvinAlvarado <ealvarado301@gmail.com>
2023-03-29 00:36:17 -0500

    patched snap-pac config. added a fail safe. formatting

--- Arch/arch-post-reboot.sh
+++ Arch/arch-post-reboot.sh
@@ -45,16 +45,14 @@ sudo btrfs subvolume delete /.snapshots
 sudo mkdir /.snapshots
 sudo mount -a
 sudo chmod 750 /.snapshots
-
 # Setup home config
 sudo snapper -c home create-config /home
-
 # Activate automatic snapshots
 systemctl enable --now snapper-timeline.timer
 systemctl enable --now snapper-cleanup.timer
-
 # Setup pre/post root snapshots for pacman transactions
-sudo sed -i -e 's/#[root]/[root]/' /etc/snap-pac.ini
+# TODO root command did not work. added fix. not tested
+sudo sed -i -e 's/#\[root\]/\[root\]/' /etc/snap-pac.ini
 sudo sed -i -e 's/#desc_limit/desc_limit/' /etc/snap-pac.ini
 sudo sed -i -e 's/#snapshot/snapshot/' /etc/snap-pac.ini
 sudo sed -i -e 's/#cleanup/cleanup/' /etc/snap-pac.ini
@@ -63,13 +61,12 @@ sudo sed -i -e 's/#post/post/' /etc/snap-pac.ini
 sudo sed -i -e 's/#important/important/g' /etc/snap-pac.ini
 sudo sed -i -e 's/"pacman -Syu"/"pacman -Syu", "pikaur -Syu"/' /etc/snap-pac.ini
 sudo sed -i -e 's/"linux"/"linux", "linux-zen", "nvidia-utils", "nvidia-dkms", "systemd", "systemd-libs", "zram-generator", "amd-ucode", "intel-ucode", "networkmanager", "linux-firmware", "btrfs-progs"/' /etc/snap-pac.ini
+sudo vi /etc/snap-pac.ini
 
 ## Network Printing
-
 # You must disable the systemd DNS resolver
 systemctl stop systemd-resolved.service
 systemctl disable systemd-resolved.service
-
 # and use avahi and mdns for it to work.
 sudo pacman -S nss-mdns avahi samba
 systemctl enable --now avahi-daemon.service
