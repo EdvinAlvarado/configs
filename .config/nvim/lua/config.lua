1ac57ab0fad6f8f76fe3ec4dbb3e8ebee14e27bf
EdvinAlvarado <ealvarado301@gmail.com>
2023-09-27 21:05:13 -0500

    removed go lua stuff

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -15,15 +15,6 @@ rt.setup({
 	},
 })
 
--- go
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*.go",
-	callback = function()
-		require('go.format').goimport()
-	end,
-	group = format_sync_grp,
-})
 
 -- Mason LSP Package Manager
 require("mason").setup()
