36fc1b50ade273492057b2822bfefece3c5af83b
EdvinAlvarado <ealvarado301@gmail.com>
2023-10-02 00:50:59 -0500

    neovim: change vim-go for go.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -335,19 +335,46 @@ require("lazy").setup({
 	end,
 },
 {
+--{
 -- Adds go language support: e.g.
 		-- :GoBuild
 		-- :GoTest
 		-- :GoInstall
 		-- :GoRun
 -- Requires gopls
-	'fatih/vim-go',
-	build = ':GoUpdateBinaries',
-	event = {"BufRead *.go"},
+--	'fatih/vim-go',
+--	build = ':GoUpdateBinaries',
+--	event = {"BufRead *.go"},
+--	config = function()
+--		vim.g.go_def_mode = 'gopls'
+--		vim.g.go_info_mode = 'gopls'
+--		vim.g.go_gopls_enabled = 0
+--	end,
+--},
+{
+	"ray-x/go.nvim",
+	dependencies = {	-- optional packages
+		"ray-x/guihua.lua",
+		"neovim/nvim-lspconfig",
+		"nvim-treesitter/nvim-treesitter",
+	},
 	config = function()
-		vim.g.go_def_mode = 'gopls'
-		vim.g.go_info_mode = 'gopls'
+		require("go").setup()
+		require("go.format").goimport()  -- goimport + gofmt
+
+		-- Run gofmt + goimport on save
+		local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+		  pattern = "*.go",
+		  callback = function()
+		   require('go.format').goimport()
+		  end,
+		  group = format_sync_grp,
+		})
 	end,
+	event = {"CmdlineEnter"},
+	ft = {"go", 'gomod'},
+	build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
 },
 
 {
