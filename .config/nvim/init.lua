3c890badb81aa30e2dcd6e6e1f78105c21015857
EdvinAlvarado <ealvarado301@gmail.com>
2023-10-02 00:51:20 -0500

    neovim: added more fuzzy finder shortcuts

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -170,12 +170,12 @@ require("lazy").setup({
 	},
 	config = function()
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
-		vim.keymap.set('n', '<leader>fc', builtin.commands, {})
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'fuzzy finder files'})
+		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'fuzzy finder lines'})
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'fuzzy finder buffers'})
+		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'fuzzy finder help'})
+		vim.keymap.set('n', '<leader>ft', builtin.treesitter, {desc = 'fuzzy finder tresitter'})
+		vim.keymap.set('n', '<leader>fc', builtin.commands, {desc = 'fuzzy finder commands'})
 	end,
 },
 {
