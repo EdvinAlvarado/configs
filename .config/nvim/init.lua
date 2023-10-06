8c72c9e79871143bbb242e328637240d12889422
EdvinAlvarado <ealvarado301@gmail.com>
2023-10-06 00:22:39 -0500

    neovim: adding ziglang/zig.vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -396,6 +396,17 @@ require("lazy").setup({
 		require('neodev').setup()
 	end
 },
+{
+-- zig file detection and syntaz highlighing
+	'ziglang/zig.vim',
+	ft = "zig",
+	config = function()
+		vim.keymap.set('n', '<c-b>', '<cmd>w<cr>'..'<cmd>compiler zig_build<cr>')
+		vim.keymap.set('n', '<c-t>', '<cmd>w<cr>'..'<cmd>compiler zig_test<cr>')
+		vim.keymap.set('n', '<c-r>', '<cmd>w<cr>'..'<cmd>compiler zig_build_exe<cr>')
+	end
+
+},
 -- Add gutter with git status
 	'airblade/vim-gitgutter',
 -- git commandsm :Git
