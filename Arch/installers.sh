acfae918c7859921a1d14b504885f9fac3814b91
Edvin Alvarado <ealvarado301@gmail.com>
2026-07-25 18:36:11 -0500

    refactor

--- Arch/installers.sh
+++ Arch/installers.sh
@@ -15,19 +15,19 @@ cd .. && \
 rm -rf cachyos-repo.tar.xz cachyos-repo
 
 ## chaotic-aur
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && \
+sudo pacman-key --lsign-key 3056513887B78AEB && \
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' && \
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && \
+echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf && \
+echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf && \
 sudo pacman --noconfirm -Syu
 
 ## paru 
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg --noconfirm -si
-cd ..
-rm -rf paru
+git clone https://aur.archlinux.org/paru.git && \
+cd paru && \
+makepkg --noconfirm -si && \
+cd .. && \
+rm -rf paru && \
 paru --gendb
 
