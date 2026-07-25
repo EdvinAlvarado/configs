325c7513cbbaee2c8ae4573d365f1fecf21dd952
Edvin Alvarado <ealvarado301@gmail.com>
2026-07-25 18:34:45 -0500

    clear pacman cache

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 sudo pacman -Syu && pacman -Qdtq | sudo pacman -Rnsc -
-paru -Syu
+paru -Syu && paru -Scc 
+sudo paccache -ruk0
+sudo paccache -rk 2
 flatpak update && flatpak uninstall --unused
 ya pkg upgrade --discard
 tldr --update
