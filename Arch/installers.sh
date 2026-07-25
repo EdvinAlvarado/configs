bce731ed3d106bf9ca405371036af105c3cdca2f
Edvin Alvarado <ealvarado301@gmail.com>
2026-07-25 18:36:04 -0500

    implementing cachyos kernel and packages

--- Arch/installers.sh
+++ Arch/installers.sh
@@ -5,6 +5,15 @@ sudo sed -i -e "s/#ParallelDownloads = 5/ParallelDownloads = 10/" /etc/pacman.co
 sudo sed -i -E 's/^(CFLAGS="-march=)\w+/\1native/' /etc/makepkg.conf
 sudo sed -i -E 's/^(RUSTFLAGS=".+)"/\1 -C target-cpu=native"/' /etc/makepkg.conf.d/rust.conf
 
+## cahyos-kernel
+# Download and extract the installer
+curl -O https://mirror.cachyos.org/cachyos-repo.tar.xz && \
+tar xvf cachyos-repo.tar.xz && cd cachyos-repo && \
+# Run the automated installer
+sudo ./cachyos-repo.sh && \
+cd .. && \
+rm -rf cachyos-repo.tar.xz cachyos-repo
+
 ## chaotic-aur
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
