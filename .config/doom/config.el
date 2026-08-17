b4f507f1da3adfea8982637666c004a73ac65288
Edvin Alvarado <ealvarado301@gmail.com>
2026-08-16 21:30:30 -0500

    doom emacs - linting

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
- (setopt user-full-name "Edvin Alvarado"
-       user-mail-address "ealvarado301@gmail.com")
+(setopt user-full-name "Edvin Alvarado"
+        user-mail-address "ealvarado301@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
