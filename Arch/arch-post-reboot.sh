23ef1de92c0e513860d331b402724776b878287c
Edvin Alvarado <ealvarado301@gmail.com>
2026-09-01 22:36:47 -0500

    linting

--- Arch/arch-post-reboot.sh
+++ Arch/arch-post-reboot.sh
@@ -1,5 +1,7 @@
-## CLI 
-# Add yazi plugins 
+#!/usr/bin/env bash
+
+## CLI
+# Add yazi plugins
 ya pkg add yazi-rs/flavors:dracula
 ya pkg add yazi-rs/plugins:smart-enter
 ya pkg add yazi-rs/plugins:mount
@@ -14,28 +16,28 @@ fish -c "fisher install jorgebucaran/fisher"
 # Tealdeer
 tldr --update
 # wikiman
-curl -L 'https://raw.githubusercontent.com/filiparag/wikiman/master/Makefile' -o 'wikiman-makefile' && \
-make -f ./wikiman-makefile source-arch && \
-make -f ./wikiman-makefile source-tldr && \
-sudo make -f ./wikiman-makefile source-install && \
-sudo make -f ./wikiman-makefile clean
+curl -L 'https://raw.githubusercontent.com/filiparag/wikiman/master/Makefile' -o 'wikiman-makefile' &&
+	make -f ./wikiman-makefile source-arch &&
+	make -f ./wikiman-makefile source-tldr &&
+	sudo make -f ./wikiman-makefile source-install &&
+	sudo make -f ./wikiman-makefile clean
 # Rust
-rustup default nightly && \
-rustup component add rust-analyzer rustfmt rust-src clippy && \
-cargo install cargo-seek
+rustup default stable &&
+	rustup component add rust-analyzer rustfmt rust-src clippy &&
+	cargo install cargo-seek
 # Haskell
-paru --noconfirm -S ghcup-hs-bin && \
-ghcup install ghc && \
-ghcup install cabal && \
-ghcup install hls && \
-ghcup install stack && \
-ghcup install ShellCheck && \
-cabal install fast-tags
+paru --noconfirm -S ghcup-hs-bin &&
+	ghcup install ghc &&
+	ghcup install cabal &&
+	ghcup install hls &&
+	ghcup install stack &&
+	ghcup install ShellCheck &&
+	cabal install fast-tags
 # Common Lisp
 sbcl --load /usr/share/quicklisp/quicklisp.lisp \
-     --eval '(quicklisp-quickstart:install)' \
-     --eval '(ql:add-to-init-file)' \
-     --quit
+	--eval '(quicklisp-quickstart:install)' \
+	--eval '(ql:add-to-init-file)' \
+	--quit
 # Clojure
 paru --noconfirm -S clojure-lsp-bin clj-kondo-bin babashka-bin
 # doom emacs
@@ -46,8 +48,8 @@ git clone --depth 1 https://github.com/doomemacs/core ~/.config/emacs
 paru --noconfirm -S limine-mkinitcpio-hook limine-snapper-sync
 sudo limine-update
 # archiso
-paru -S archiso-systemd-boot && \
-sudo cat <<EOF >> /boot/limine.conf
+paru -S archiso-systemd-boot &&
+	sudo cat <<EOF >>/boot/limine.conf
 
 
 /Arch Linux Rescue
@@ -59,17 +61,16 @@ module_path: boot():/archiso/initramfs-linux.img
 cmdline: archisobasedir=archiso archisosearchfilename=/archiso/vmlinuz-linux 
 EOF
 
-
-## Applications 
+## Applications
 sudo pacman --noconfirm -S neofetch insync anki ledger-live ventoy-bin bambustudio-bin orca-slicer-bin ocrmypdf qdirstat subtitleedit trayscale otf-symbola
 paru --noconfirm -S proton-mail proton-vpn-gtk-app
 paru --noconfirm -S fcitx5-mozc-ut fcitx5-breeze
-paru --noconfirm -S wike-tui 
-paru --noconfirm -S rusty-man 
-paru --noconfirm -S cargo-info 
+paru --noconfirm -S wike-tui
+paru --noconfirm -S rusty-man
+paru --noconfirm -S cargo-info
 paru --noconfirm -S frame-eth
-paru --noconfirm -S libation 
-paru --noconfirm -S audiobookconverter-bin 
+paru --noconfirm -S libation
+paru --noconfirm -S audiobookconverter-bin
 
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
 	ESPANSO="espanso-wayland"
@@ -101,27 +102,32 @@ sudo sed -i -e 's/#important/important/g' /etc/snap-pac.ini
 sudo sed -i -e 's/"pacman -Syu"/"pacman -Syu", "paru -Syu"/' /etc/snap-pac.ini
 sudo sed -i -e 's/"linux"/"linux", "linux-zen", "nvidia-utils", "nvidia-dkms", "systemd", "systemd-libs", "zram-generator", "amd-ucode", "intel-ucode", "networkmanager", "linux-firmware", "btrfs-progs"/' /etc/snap-pac.ini
 
-
 ## Configuration
 sudo systemctl mask systemd-udev-settle
 
 ## AI
 # Setup Kagi MCP Server
-cd ~/Projects && \
-git clone https://github.com/kagisearch/kagimcp.git && \
-cd kagicmp && \
-uv sync
+cd ~/Projects &&
+	git clone https://github.com/kagisearch/kagimcp.git &&
+	cd kagicmp &&
+	uv sync
 # Setup Ollama
 while true; do
 	read -p "Nvidia or AMD GPU? (e.g. nvidia, amd, none) " yn
 	case $yn in
-		[nvidiaNVIDIA]* ) sudo pacman -S ollama-cuda;
-				break;;
-		[amdAMD]* ) sudo pacman -S ollama-rocm;
-				break;;
-		[Nn]* ) echo "NOTICE: install ollama-cuda (for nvidia) or ollama-rocm (for AMD) for ollama use.";
-			break;;
-		*     ) echo "Which one?";;
+	[nvidiaNVIDIA]*)
+		sudo pacman -S ollama-cuda
+		break
+		;;
+	[amdAMD]*)
+		sudo pacman -S ollama-rocm
+		break
+		;;
+	[Nn]*)
+		echo "NOTICE: install ollama-cuda (for nvidia) or ollama-rocm (for AMD) for ollama use."
+		break
+		;;
+	*) echo "Which one?" ;;
 	esac
 done
 
--- Arch/installers.sh
+++ Arch/installers.sh
@@ -5,29 +5,28 @@ sudo sed -i -e "s/#ParallelDownloads = 5/ParallelDownloads = 10/" /etc/pacman.co
 sudo sed -i -E 's/^(CFLAGS="-march=)\w+/\1native/' /etc/makepkg.conf
 sudo sed -i -E 's/^(RUSTFLAGS=".+)"/\1 -C target-cpu=native"/' /etc/makepkg.conf.d/rust.conf
 
-## cahyos-kernel
+## cachyos-kernel
 # Download and extract the installer
-curl -O https://mirror.cachyos.org/cachyos-repo.tar.xz && \
-tar xvf cachyos-repo.tar.xz && cd cachyos-repo && \
-# Run the automated installer
-sudo ./cachyos-repo.sh && \
-cd .. && \
-rm -rf cachyos-repo.tar.xz cachyos-repo
+curl -O https://mirror.cachyos.org/cachyos-repo.tar.xz &&
+	tar xvf cachyos-repo.tar.xz && cd cachyos-repo &&
+	# Run the automated installer
+	sudo ./cachyos-repo.sh &&
+	cd .. &&
+	rm -rf cachyos-repo.tar.xz cachyos-repo
 
 ## chaotic-aur
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && \
-sudo pacman-key --lsign-key 3056513887B78AEB && \
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' && \
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && \
-echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf && \
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf && \
-sudo pacman --noconfirm -Syu
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com &&
+	sudo pacman-key --lsign-key 3056513887B78AEB &&
+	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' &&
+	sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' &&
+	echo "[chaotic-aur]" | sudo tee -a /etc/pacman.conf &&
+	echo "Include = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf &&
+	sudo pacman --noconfirm -Syu
 
-## paru 
-git clone https://aur.archlinux.org/paru.git && \
-cd paru && \
-makepkg --noconfirm -si && \
-cd .. && \
-rm -rf paru && \
-paru --gendb
-
+## paru
+git clone https://aur.archlinux.org/paru.git &&
+	cd paru &&
+	makepkg --noconfirm -si &&
+	cd .. &&
+	rm -rf paru &&
+	paru --gendb
